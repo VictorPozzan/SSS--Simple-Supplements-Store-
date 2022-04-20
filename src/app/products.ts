8b61ca86676f710969193d606ced8d40b496c298
VictorPozzan <victor_pozzan@hotmail.com>
2022-04-20 00:36:42 -0300

    1 file modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -12,30 +12,40 @@ export const products = [
     name: 'Vitaminas',
     price: 69,
     description: 'Complexo de vitaminas A, B, C, D e E',
+    image:
+      'https://stackblitz.com/files/angular-ivy-b73e62/github/VictorPozzan/SSS--Simple-Supplements-Store-/master/src/assets/images/vitamins.png',
   },
   {
     id: 3,
     name: 'Maca Peruana',
     price: 29,
     description: '',
+    image:
+      'https://stackblitz.com/files/angular-ivy-b73e62/github/VictorPozzan/SSS--Simple-Supplements-Store-/master/src/assets/images/maca_peruana.png',
   },
   {
     id: 4,
     name: 'Creatina',
     price: 29,
     description: '',
+    image:
+      'https://stackblitz.com/files/angular-ivy-b73e62/github/VictorPozzan/SSS--Simple-Supplements-Store-/master/src/assets/images/creatine.png',
   },
   {
     id: 5,
     name: 'Cafeína',
     price: 29,
     description: '',
+    image:
+      'https://stackblitz.com/files/angular-ivy-b73e62/github/VictorPozzan/SSS--Simple-Supplements-Store-/master/src/assets/images/pills-cafeine.png',
   },
   {
     id: 6,
     name: 'BCAA',
     price: 29,
     description: '',
+    image:
+      'https://stackblitz.com/files/angular-ivy-b73e62/github/VictorPozzan/SSS--Simple-Supplements-Store-/master/src/assets/images/bcaa.png',
   },
 ];
 
