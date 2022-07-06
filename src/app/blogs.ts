f24fa30d953efff348fd6a3348dc4f412d9a5347
VictorPozzan <victor_pozzan@hotmail.com>
2022-07-05 22:46:10 -0300

    1 file modified, 1 added

---
+++ src/app/blogs.ts
@@ -0,0 +1,9 @@
+export const blog = [
+  {
+    title: 'Sport Nutrition',
+    subTitle: 'Run, swim, bike and play with much more intensity',
+    description: '',
+    link: '',
+    image: '',
+  },
+];
