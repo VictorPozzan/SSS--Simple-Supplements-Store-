d15dec033149a2dd119c64206469c2177e7b0ea3
VictorPozzan <32144188+VictorPozzan@users.noreply.github.com>
2022-11-22 21:30:03 -0300

    3 files modified

--- src/app/blogs.ts
+++ src/app/blogs.ts
@@ -5,7 +5,8 @@ export const blogs = [
     description:
       "Sports Nutrition is the study of science and nutrition that helps you achieve better performance in your sport. It's a science that looks at how your body uses energy during exercise, how to maintain fluid balance, how to store and use carbohydrates and how to use protein to build and repair muscles.",
     link: '',
-    image: '',
+    image:
+      'https://github.com/VictorPozzan/SSS--Simple-Supplements-Store-/blob/master/src/assets/images/nutrition.png?raw=true',
     color: '#B73E3E',
   },
   {
@@ -14,7 +15,8 @@ export const blogs = [
     description:
       'Nutritional yeast is a deactivated yeast used as a food ingredient. It is rich in protein, fiber and vitamins. It has a slightly cheesy flavor. Here are the 10 best substitutes for Nutritional Yeast.',
     link: '',
-    image: '',
+    image:
+      'https://github.com/VictorPozzan/SSS--Simple-Supplements-Store-/blob/master/src/assets/images/balanced-diet.png?raw=true',
     color: '#665A48',
   },
 ];
